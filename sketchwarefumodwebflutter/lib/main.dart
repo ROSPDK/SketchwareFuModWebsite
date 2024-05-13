d2d8b594454634f4d49ef92e61c8bb564242cd3e
ROSPDK <130581081+ROSPDK@users.noreply.github.com>
2024-05-13 10:56:19 -0300

    Update main.dart

--- sketchwarefumodwebflutter/lib/main.dart
+++ sketchwarefumodwebflutter/lib/main.dart
@@ -45,6 +45,15 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       drawer: NavigationDrawer(children: [
+          DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                //image: AssetImage("assets/flutter.jpg"),
+                     fit: BoxFit.cover)
+              ),
+            ),
           const SizedBox(height: 22),
           for (var i = 0; i < 30; i++)
             NavigationDrawerDestination(
