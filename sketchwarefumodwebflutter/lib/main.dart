063fa191a01cfe6f57691e5326e8b0d3ed643875
ROSPDK <130581081+ROSPDK@users.noreply.github.com>
2024-05-15 22:32:43 -0300

    Update main.dart

--- sketchwarefumodwebflutter/lib/main.dart
+++ sketchwarefumodwebflutter/lib/main.dart
@@ -59,8 +59,8 @@ class _MyHomePageState extends State<MyHomePage> {
               label: Text('Item $i'),
             ),
         ]),
-      body: Column(
-        //child: Column(
+      body: SingleChildScrollView(
+        child: Column(
           //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      //),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
