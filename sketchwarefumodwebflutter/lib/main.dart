5d7c216bc4742a864620936a94823c39167791c2
ROSPDK <130581081+ROSPDK@users.noreply.github.com>
2024-05-15 22:27:04 -0300

    Update main.dart

--- sketchwarefumodwebflutter/lib/main.dart
+++ sketchwarefumodwebflutter/lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ]),
       body: Column(
         //child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
