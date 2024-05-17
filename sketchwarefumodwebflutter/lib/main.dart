d055cd7b5fcbae6c7d190b0482981e1b46bed50b
ROSPDK <130581081+ROSPDK@users.noreply.github.com>
2024-05-16 22:51:25 -0300

    Fix main.dart

--- sketchwarefumodwebflutter/lib/main.dart
+++ sketchwarefumodwebflutter/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sketchware Fu Mod',
       theme: ThemeData(
-        fontFamily: 'GoogleSans'
+        fontFamily: 'GoogleSans',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
