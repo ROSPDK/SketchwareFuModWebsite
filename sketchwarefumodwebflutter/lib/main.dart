01c52da5c256d153151ed38e2ba057843bf59134
ROSPDK <130581081+ROSPDK@users.noreply.github.com>
2024-05-16 22:50:41 -0300

    feat: Google Sans Font

--- sketchwarefumodwebflutter/lib/main.dart
+++ sketchwarefumodwebflutter/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sketchware Fu Mod',
       theme: ThemeData(
+        fontFamily: 'GoogleSans'
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
